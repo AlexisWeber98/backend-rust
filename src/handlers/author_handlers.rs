fa0962114db904d7563e70d87e02f96fdb314194
Ale David Weber <Alexis.Weber@mailfence.com>
2024-07-08 15:04:16 -0300

    fix error update mongodb, add get author byid function

--- src/handlers/author_handlers.rs
+++ src/handlers/author_handlers.rs
@@ -1,8 +1,10 @@
+use mongodb::bson::doc;
 use actix_web::{get, post, web, HttpResponse, Responder};
 use futures::stream::TryStreamExt;
+use mongodb::bson::oid::ObjectId;
 use std::sync::Mutex;
 
-use crate::models::author::{Author, ApiResponse};
+use crate::models::author::{Author, AuthorDb, ApiResponse};
 use crate::state::AppState;
 
 #[post("/author")]
@@ -16,7 +18,7 @@ pub async fn add_author(
 
     let new_author_inner = new_author.into_inner();
 
-    match collection.insert_one(&new_author_inner, None).await {
+    match collection.insert_one(&new_author_inner).await {
         Ok(_) => {
             let response = ApiResponse {
                 result: "success".to_string(),
@@ -30,24 +32,52 @@ pub async fn add_author(
     }
 }
 
-#[get("/authors")]
-pub async fn get_all(data: web::Data<Mutex<AppState>>) -> impl Responder {
+#[get("/author/{id}")]
+pub async fn get_author(data: web::Data<Mutex<AppState>>, path: web::Path<String>) -> impl Responder {
     let client = &data.lock().unwrap().client;
     let db = client.database("rustserver");
     let collection = db.collection::<Author>("authors");
 
-    match collection.find(None, None).await {
-        Ok(cursor) => {
-            let authors: Vec<Author> = cursor.try_collect().await.unwrap_or_else(|e| {
-                println!("Failed to get authors: {}", e);
-                Vec::new()
-            });
 
-            HttpResponse::Ok().json(authors)
+    let object_id = match ObjectId::parse_str(&path.into_inner()) {
+        Ok(id) => id,
+        Err(_) => return HttpResponse::BadRequest().body("Invalid ID format"),
+    };
+
+    match collection.find_one(doc! { "_id": object_id }).await {
+        Ok(Some(author)) => {
+            let response = ApiResponse {
+                result: "success".to_string(),
+                data: author,
+            };
+            HttpResponse::Ok().json(response)
         }
-        Err(e) => {
-            println!("Failed to get authors: {}", e);
-            HttpResponse::InternalServerError().finish()
-        }
+        Ok(None) => HttpResponse::NotFound().body("Author not found"),
+        Err(error) => HttpResponse::InternalServerError().body(format!("Failed to get author: {}", error)),
     }
 }
+
+
+
+
+#[get("/authors")]
+pub async fn get_all(data: web::Data<Mutex<AppState>>) -> impl Responder {
+    let client = &data.lock().unwrap().client;
+    let db = client.database("rustserver");
+    let collection = db.collection::<AuthorDb>("authors");
+
+    let authors = match collection.find(doc!{}).await {
+        Ok(cursor) => cursor.try_collect::<Vec<AuthorDb>>().await.unwrap_or_else(|e| {
+                println!("Failed to get authors: {}", e);
+                Vec::new()
+            }),
+            Err(e) => {
+                println!("failed to get authors: {}", e);
+                return HttpResponse::InternalServerError().finish();
+            }
+
+    };
+    HttpResponse::Ok().json(authors)
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ mod handlers;
 mod models;
 mod state;
 
-use handlers::author_handlers::{add_author, get_all};
+use handlers::author_handlers::{add_author, get_all, get_author};
 use state::AppState;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
+    let client_options = ClientOptions::parse("mongodb+srv://Rust-Server:Rust-Server@rustserver.a5c2xn3.mongodb.net/?retryWrites=true&w=majority&appName=RustServer").await.unwrap();
     let client = Client::with_options(client_options).unwrap();
     let state = web::Data::new(Mutex::new(AppState { client }));
 
@@ -20,6 +20,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(state.clone())
             .service(add_author)
             .service(get_all)
+            .service(get_author)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
